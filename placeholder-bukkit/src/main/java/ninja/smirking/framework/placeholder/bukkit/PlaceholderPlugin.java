b3c8bd371077e7f0e900d1dd62bed5092153ae29
Connor Spencer Harries <connor@smirking.ninja>
2015-12-14 14:58:27 +0000

    Release placeholders associated with a plugin when it is disabled.

--- placeholder-bukkit/src/main/java/ninja/smirking/framework/placeholder/bukkit/PlaceholderPlugin.java
+++ placeholder-bukkit/src/main/java/ninja/smirking/framework/placeholder/bukkit/PlaceholderPlugin.java
@@ -2,23 +2,31 @@ package ninja.smirking.framework.placeholder.bukkit;
 
 import ninja.smirking.framework.placeholder.api.PlaceholderManager;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlaceholderPlugin extends JavaPlugin {
     private BukkitPlaceholderManager placeholderManager;
+    private PluginListener pluginListener;
 
     @Override
     public void onDisable() {
         getServer().getServicesManager().unregister(BukkitPlaceholderManager.class, placeholderManager);
         getServer().getServicesManager().unregister(PlaceholderManager.class, placeholderManager);
+        HandlerList.unregisterAll(pluginListener);
         placeholderManager.release();
         placeholderManager = null;
+        pluginListener = null;
     }
 
     @Override
     public void onEnable() {
         placeholderManager = new BukkitPlaceholderManager();
+        pluginListener = new PluginListener(this);
         for (BukkitPlaceholder placeholder : BukkitPlaceholder.values()) {
             placeholderManager.registerMapping(this, placeholder.getPlaceholder(), placeholder.getMappingFunction());
             for (String alias : placeholder.getAliases()) {
@@ -27,9 +35,23 @@ public final class PlaceholderPlugin extends JavaPlugin {
         }
         getServer().getServicesManager().register(BukkitPlaceholderManager.class, placeholderManager, this, ServicePriority.Highest);
         getServer().getServicesManager().register(PlaceholderManager.class, placeholderManager, this, ServicePriority.Highest);
+        getServer().getPluginManager().registerEvents(pluginListener, this);
     }
 
     public BukkitPlaceholderManager getPlaceholderManager() {
         return placeholderManager;
+    }
+
+    public class PluginListener implements Listener {
+        private final PlaceholderPlugin plugin;
+
+        public PluginListener(PlaceholderPlugin plugin) {
+            this.plugin = plugin;
+        }
+
+        @EventHandler
+        public void onPluginDisable(PluginDisableEvent event) {
+            plugin.getPlaceholderManager().unregisterMappings(event.getPlugin());
+        }
     }
 }
